71b9b47cc43885a62e028036e01c188491a1543a
EnigmaCurry <ryan@enigmacurry.com>
2026-01-18 13:12:42 -0700

    deps

--- tt-tui-for-traefik/src/cli.rs
+++ tt-tui-for-traefik/src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{Arg, Command};
 
 pub fn app() -> Command {
-    Command::new("tt-tui-for-traefik")
+    Command::new(env!("CARGO_BIN_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
